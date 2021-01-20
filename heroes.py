513e70b98699092f5d0621122ca7e3f0ab4da11e
Герман <german@MacBook-Air-German.local>
2021-01-20 18:49:22 +0300

    new changes

--- heroes.py
+++ heroes.py
@@ -5,12 +5,14 @@ def max_intelligence():
     token = input('Введите токен: ')
     token = int(token)
     list_heroes = {}
-    for id_number in range(1,732):
+    for id_number in range(1,10):
         data = requests.get(f'https://superheroapi.com/api/{token}/{id_number}/powerstats/get')
     
         if data.status_code != 200:
             raise Exception('не работает')
         hero_int = data.json()['intelligence']
+        if hero_int == 'null':
+            continue
         hero_name = data.json()['name']
         list_heroes[hero_int] = hero_name
     list_heroes=sorted(list_heroes.items(), key=operator.itemgetter(1), reverse=True) 
